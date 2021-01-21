3f48d744e7c8715a75444a79caafe121cee12330
Tanner1638 <tanner1638@gmail.com>
2021-01-20 23:17:07 -0600

    Added basic member counter
    removed message when user leaves temporarily

---
+++ counters/membercounter.js
@@ -0,0 +1,11 @@
+module.exports = async (client) => {
+    const guild = client.guilds.cache.get('783496949427863563');
+    setInterval(() => {
+        const memberCount = guild.memberCount;
+        const channel = guild.channels.cache.get('801677980194963476');
+
+        channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
+        console.log('Updating Member Count');
+    }, 5000);
+    
+}
